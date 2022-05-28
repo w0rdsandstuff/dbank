78e3534bfdd7a7cdf830c371bfd2bf1b709f56c5
lorenzo <77218212+w0rdsandstuff@users.noreply.github.com>
2022-05-28 17:16:08 -0500

    Add files via upload

---
+++ src/dbank/main.mo
@@ -0,0 +1,44 @@
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  stable var currentValue: Float = 300;
+  Debug.print(debug_show(currentValue));
+  
+  stable var startTime = Time.now();
+  Debug.print(debug_show(startTime));
+
+  Debug.print(debug_show(currentValue));
+
+  public func topUp(amount: Float) {
+    currentValue += amount;
+    Debug.print(debug_show(currentValue));
+  };
+
+  public func withdrawal(amount: Float) {
+    let tempValue: Float = currentValue - amount;
+    if (tempValue - amount >= 0) { 
+    currentValue -= amount;
+    Debug.print(debug_show(currentValue));
+  } else {
+    Debug.print("Amount too large, currentValue less than zero");
+  }
+};
+
+public query func checkBalance(): async Float  {
+  return currentValue;
+};
+
+
+//  topUp();
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsedNS = currentTime - startTime;
+    let timeElapsedS = timeElapsedNS / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsedS));
+    startTime := currentTime;
+  }; 
+} 
+
---
+++ src/dbank_assets/src/index.js
@@ -0,0 +1,36 @@
+import { dbank } from '../../declarations/dbank'
+
+window.addEventListener('load', async function() {
+  const currentAmount = await dbank.checkBalance(); 
+  document.getElementById('value').innerText = Math.round(currentAmount * 100) / 100;
+});
+
+document.querySelector('form').addEventListener('submit', async function(event) {
+  event.preventDefault();
+  
+  const button = event.target.querySelector('#submit-btn');
+
+  const inputAmount =  parseFloat(document.getElementById('input-amount').value);
+  const outputAmount = parseFloat(document.getElementById('withdrawal-amount').value);
+  
+  button.setAttribute('disabled', true)
+  
+  if (document.getElementById('input-amount').value.length != 0) {
+  await dbank.topUp(inputAmount);
+}
+  
+  if (document.getElementById('withdrawal-amount').value.length != 0) {
+    await dbank.withdrawal(outputAmount);
+  }
+
+//  await dbank.compound();
+
+  const currentAmount = await dbank.checkBalance(); 
+  document.getElementById('value').innerText = Math.round(currentAmount * 100) / 100;
+  
+  document.getElementById('input-amount').value = '';
+  document.getElementById('withdrawal-amount').value = '';
+
+  button.removeAttribute('disabled');
+
+});
